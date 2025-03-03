b9914ef83d34b51bfd78dc13d617674aec088989
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-03-03 15:28:27 +0700

    step 3 2

--- app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,12 +1,23 @@
 package com.example.compose.rally
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
+import org.junit.Test
 
 class TopAppBarTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    // TODO: Add tests
+    @Test
+    fun rallyTopAppBarTest() {
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = ,
+                onTabSelected = { /*TODO*/ },
+                currentScreen =
+            )
+        }
+    }
 }
